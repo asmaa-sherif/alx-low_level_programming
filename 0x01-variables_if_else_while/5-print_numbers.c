0ed383b500fcec5eb55d248c9fb98fd353c968fe
Asmaa Sherif Ahmed <64560423+asmaa-sherif@users.noreply.github.com>
2023-02-17 22:54:54 +0200

    5. Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -14,9 +14,10 @@ int main(void)
 {
     int num;
     
-    for(num = 0; num <= 9; num++)
+    for (num = 0; num < 10; num++)
             printf("%d", num);
   
-    printf('\n');
+    printf("\n");
+    
     return (0);
 }
