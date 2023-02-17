0356afff378e5d532b9c171c348a96680d6f533c
Asmaa Sherif Ahmed <64560423+asmaa-sherif@users.noreply.github.com>
2023-02-17 21:55:41 +0200

    2. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,24 @@
+/*
+ * File: 2-print_alphabet.c
+ * Auth: Asmaa Sherif
+ */
+
 #include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
+int main (void)
+{
+  
+  char alphabet_lowercase;
+  
+    for (alphabet_lowercase = "a"; alphabet_lowercase <= "z"; alphabet_lowercase++)
+           putchar(alphabet_lowercase);
+  
+    putchar('\n');
+  
+    return(0);
+}
