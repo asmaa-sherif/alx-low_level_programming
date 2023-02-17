8b22ace22154798518aa3f0148328575bbf5d4fc
Asmaa Sherif Ahmed <64560423+asmaa-sherif@users.noreply.github.com>
2023-02-17 23:53:11 +0200

    8. Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -20,7 +20,7 @@ int main(void)
 	for (num = 0; num <= 9; num++)
 		putchar((num % 10)+ '0');
 
-	for (letter = 'a'; letter <= 'f'; lette++)
+	for (letter = 'a'; letter <= 'f'; letter++)
 		putchar(letter);
 
 	putchar('\n');
