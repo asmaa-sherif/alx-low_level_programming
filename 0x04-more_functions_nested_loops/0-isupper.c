e3bb4d9c24c17e4d584419c60f6194717046c169
Asmaa Sherif Ahmed <64560423+asmaa-sherif@users.noreply.github.com>
2023-02-24 00:40:42 +0200

    0. isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,19 +4,19 @@
  */
 
 #include "main.h"
-#include < stdio.h>
 
 /* main - check the code.
  *
  * Return: Always 0.
  */
-int main(void)
+int _isupper(int c);
 {
 	char c;
 
-	c = 'A';
-	printf('%c : %d\n', c , _isupper(c));
-	c = 'a';
-	printf('%c : %d\n', c , _isupper(c));
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	
 	return(0);
 }
