efe2657997ff971d7f544a0368dfe3625a703986
asmaa-sherif <asmaasherif22@gmail.com>
2023-02-23 12:25:42 -0800

    0. Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,22 @@
+/*
+ * File: 0-main.c
+ * Auth: Asmaa Sherif
+ *
+ */
+
+#include "main.h"
+
+/**
+ * main - test positive or negative function
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i ;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
