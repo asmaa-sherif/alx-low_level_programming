dd7f6f4b99980ea3dfd3758142da9aa0ca452d3d
Asmaa Sherif Ahmed <64560423+asmaa-sherif@users.noreply.github.com>
2023-03-01 23:38:42 +0200

    11. It is the addition of strangeness to beauty that constitutes the romantic character in art

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,4 +1,3 @@
-#include "main.h"
 /**
  * infinite_add - Add up two numbers stored in given char arrays
  * @n1: The first number
@@ -8,6 +7,7 @@
  *
  * Return: 0 if buffer too small to store result, else return pointer to buffer
  */
+#include "main.h"
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int l1, l2, tmpl, rl, i, sum, num1, num2, carry;
